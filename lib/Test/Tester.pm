f396f32410f48540b2264895cded2bc591dc7ee6
Chad Granum <exodist7@gmail.com>
2014-10-23 18:57:35 +0000

    Import of EXODIST/Test-Simple-1.301001_063 from CPAN.
    gitpan-cpan-distribution: Test-Simple
    gitpan-cpan-version:      1.301001_063
    gitpan-cpan-path:         EXODIST/Test-Simple-1.301001_063.tar.gz
    gitpan-cpan-author:       EXODIST
    gitpan-cpan-maturity:     developer

--- lib/Test/Tester.pm
+++ lib/Test/Tester.pm
@@ -15,7 +15,7 @@ require Exporter;
 
 use vars qw( @ISA @EXPORT $VERSION );
 
-our $VERSION = '1.301001_062';
+our $VERSION = '1.301001_063';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 @EXPORT  = qw( run_tests check_tests check_test cmp_results show_space );
@@ -169,7 +169,7 @@ sub cmp_field {
             $result->{$field},
             $expect->{$field},
         );
-        $ctx->ok($ok, "$desc compare $field"); 
+        $ctx->ok($ok, "$desc compare $field");
     }
 }
 
